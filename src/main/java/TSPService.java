f9f3ca51d28bc3fdd89cc3be982fc9e77201864f
JStewart808 <stewa149@go.stockton.edu>
2025-12-16 19:56:58 -0500

    (HALF MATT) Incorperate Frontend into Project
    Incorperated the frontend into the project. The html, css, and script.js fiels were written my Matt. The TSPService file was written by me and some fixes in the js file to make it work properly.
    
    Co-Authored-By: matthouseworth <113146213+matthouseworth@users.noreply.github.com>

---
+++ src/main/java/TSPService.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class TSPService {
+
+  public static String[] solve(String[] points) {
+    MapBuilder builder = new MapBuilder();
+    builder.buildMap("src/main/resources/FINALDATA.csv");
+
+    Map<String, Node> map = builder.getMap();
+
+    GraphBuilder graphBuilder = new GraphBuilder(map);
+    double[][] dist = graphBuilder.getDistMatrix();
+    int[][] next = graphBuilder.getNextMatrix();
+    FloydWarshall.compute(dist, next);
+
+    List<Integer> pointIndexes = new ArrayList<>();
+    for (String p : points) {
+      int idx = graphBuilder.getIndex().indexOf(p);
+      if (idx >= 0) {
+        pointIndexes.add(idx);
+      } else {
+        throw new IllegalArgumentException("Point not found in map: " + p);
+      }
+    }
+
+    double[][] newDist = GraphBuilder.buildSubMatrix(dist, pointIndexes);
+
+    int[] solution = TSPRunner.solveTSP(newDist);
+
+    String[] solutionString = new String[solution.length];
+    for (int i = 0; i < solution.length; i++) {
+      solutionString[i] = graphBuilder.getIndex().get(pointIndexes.get(solution[i]));
+    }
+
+    return solutionString;
+  }
+}
